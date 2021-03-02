1994f413cf32177ddc9ac81467888d699a9f86ed
jchaconv <jcaaron.chacon@outlook.com>
2021-03-02 17:21:07 -0500

    Adding files to the project.

---
+++ src/main/java/expert/spriframework/mcsjacksonexamples/McsjacksonexamplesApplication.java
@@ -0,0 +1,13 @@
+package expert.spriframework.mcsjacksonexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsjacksonexamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsjacksonexamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/expert/spriframework/mcsjacksonexamples/McsjacksonexamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package expert.spriframework.mcsjacksonexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsjacksonexamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
