663da1bc0b3e0cd2468cf87708b36e91bb978799
jchaconv <jcaaron.chacon@outlook.com>
2021-03-02 18:19:17 -0500

    @JsonProperty and @JsonFormat with Jackson

--- src/main/java/expert/spriframework/mcsjacksonexamples/model/BeerDto.java
+++ src/main/java/expert/spriframework/mcsjacksonexamples/model/BeerDto.java
@@ -1,5 +1,7 @@
 package expert.spriframework.mcsjacksonexamples.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,6 +22,7 @@ import java.util.UUID;
 @Builder
 public class BeerDto {
 
+    @JsonProperty("beerId")
     @Null
     private UUID id;
 
@@ -32,8 +35,12 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
+
     private OffsetDateTime lastUpdateDate;
 
 }
--- src/test/java/expert/spriframework/mcsjacksonexamples/model/BeerDtoTest.java
+++ src/test/java/expert/spriframework/mcsjacksonexamples/model/BeerDtoTest.java
@@ -26,7 +26,7 @@ class BeerDtoTest extends BaseTest {
     @Test
     void testDeserialize() throws JsonProcessingException {
 
-        String json = "{\"id\":\"f4569b87-a2ed-486d-ae27-0a911fc191c1\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":312312454534,\"price\":12.99,\"createdDate\":\"2021-03-02T17:39:51.8-05:00\",\"lastUpdateDate\":\"2021-03-02T17:39:51.8-05:00\"}";
+        String json = "{\"beerId\":\"f4569b87-a2ed-486d-ae27-0a911fc191c1\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":312312454534,\"price\":12.99,\"createdDate\":\"2021-03-02T17:39:51.8-05:00\",\"lastUpdateDate\":\"2021-03-02T17:39:51.8-05:00\"}";
 
         BeerDto dto = objectMapper.readValue(json, BeerDto.class);
 
